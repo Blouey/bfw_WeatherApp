72868a3231207d354dd72ab4647b414bb6a8f8a0
Philip Klare <philipklare@gmail.com>
2024-01-15 15:09:35 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        
+        if (!File.Exists("WetterWerte.txt"))
+        {
+            return;
+        }
+        
+        string[] allLines = File.ReadAllLines("WetterWerte.txt");
+            
+        File.WriteAllLines("C:\\Repos\\WeatherConsoleApp\\WetterResult.txt", allLines);
+    }
+}
